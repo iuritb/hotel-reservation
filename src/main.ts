93eb35a053512794b3976963c0cbab5855ad9d1f
iuri.barros <iuri.t.barros@gmail.com>
2024-07-13 16:22:46 -0300

    add unit jest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import pinia from './store';
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import pinia from "./store";
+import router from "./router";
 
-createApp(App).use(pinia).use(router).mount('#app');
+createApp(App).use(pinia).use(router).mount("#app");
--- src/mock/hotels.ts
+++ src/mock/hotels.ts
@@ -1,5 +1,5 @@
 // src/mock/hotels.ts
 export const hotels = [
-  { id: 1, name: 'Hotel A', price: 100, rating: 4.5 },
-  { id: 2, name: 'Hotel B', price: 120, rating: 4.7 },
+  { id: 1, name: "Hotel A", price: 100, rating: 4.5 },
+  { id: 2, name: "Hotel B", price: 120, rating: 4.7 },
 ];
