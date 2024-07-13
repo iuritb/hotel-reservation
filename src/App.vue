badf1d4faeed05e4b954303deafd0a90ff244c31
iuri.barros <iuri.t.barros@gmail.com>
2024-07-13 16:57:33 -0300

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
+<!-- src/App.vue -->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/search">Pesquisar Hotéis</router-link> |
+      <router-link to="/comparison">Comparar Hotéis</router-link> |
+      <router-link to="/reservation">Reservar Quartos</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
+<style>
 nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-bottom: 20px;
+}
+nav a {
+  margin-right: 10px;
+  color: #42b983;
 }
 </style>
