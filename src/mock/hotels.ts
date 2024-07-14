f1d4a9cda4cab71bcc4f7cdb4df42516e8761fe4
iuri.barros <iuri.t.barros@gmail.com>
2024-07-14 14:13:27 -0300

    create hotels data mock

--- src/mock/hotels.ts
+++ src/mock/hotels.ts
@@ -1,5 +1,37 @@
-// src/mock/hotels.ts
-export const hotels = [
-  { id: 1, name: "Hotel A", price: 100, rating: 4.5 },
-  { id: 2, name: "Hotel B", price: 120, rating: 4.7 },
+interface Hotel {
+  id: number;
+  name: string;
+  location: string;
+  rooms: number;
+  amenities: string[];
+  price: number; // Adicionando o atributo preço
+}
+
+const hotels: Hotel[] = [
+  {
+    id: 1,
+    name: "Hotel A",
+    location: "Cidade X",
+    rooms: 100,
+    amenities: ["Wi-Fi gratuito", "Estacionamento", "Academia"],
+    price: 150, // Adicionando o preço para cada hotel
+  },
+  {
+    id: 2,
+    name: "Hotel B",
+    location: "Cidade Y",
+    rooms: 200,
+    amenities: ["Wi-Fi gratuito", "Piscina", "Restaurante"],
+    price: 200,
+  },
+  {
+    id: 3,
+    name: "Hotel C",
+    location: "Cidade Z",
+    rooms: 150,
+    amenities: ["Wi-Fi gratuito", "Estacionamento", "Spa"],
+    price: 180,
+  },
 ];
+
+export default hotels;
