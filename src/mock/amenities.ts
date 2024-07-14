1d2c9be2a6084976a4cf3ffb7f836977d09ca7cf
iuri.barros <iuri.t.barros@gmail.com>
2024-07-14 14:10:01 -0300

    create amenities data mock

---
+++ src/mock/amenities.ts
@@ -0,0 +1,30 @@
+// Mock de dados para Comodidades
+interface Amenity {
+  id: number;
+  description: string;
+}
+
+const amenities: Amenity[] = [
+  {
+    id: 1,
+    description: "Wi-Fi gratuito",
+  },
+  {
+    id: 2,
+    description: "Estacionamento",
+  },
+  {
+    id: 3,
+    description: "Academia",
+  },
+  {
+    id: 4,
+    description: "Piscina",
+  },
+  {
+    id: 5,
+    description: "Piscina",
+  },
+];
+
+export default amenities;
