35f8ec71c6c3c7a6025b4d089b295c83d0c31b9a
iuri.barros <iuri.t.barros@gmail.com>
2024-07-14 18:07:02 -0300

    install and configure vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
+//Pinia
 import pinia from "./store";
+
+//Router
 import router from "./router";
 
-createApp(App).use(pinia).use(router).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(pinia).use(router).use(vuetify).mount("#app");
