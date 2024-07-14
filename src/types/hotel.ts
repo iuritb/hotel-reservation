bf56a491506686ec618eb99690294dddac6a6b29
iuri.barros <iuri.t.barros@gmail.com>
2024-07-14 15:01:46 -0300

    create mock interfaces in types

---
+++ src/types/hotel.ts
@@ -0,0 +1,8 @@
+export interface Hotel {
+  id: number;
+  name: string;
+  location: string;
+  rooms: number;
+  amenities: string[];
+  price: number;
+}
---
+++ src/types/reservation.ts
@@ -0,0 +1,8 @@
+export interface Reservation {
+  id: number;
+  hotelId: number;
+  userId: number;
+  roomId: number;
+  checkInDate: string;
+  checkOutDate: string;
+}
