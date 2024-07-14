677e8aad9121a2faae24519c77a84ff8f879391d
iuri.barros <iuri.t.barros@gmail.com>
2024-07-14 14:12:50 -0300

    create reservations data mock

---
+++ src/mock/reservations.ts
@@ -0,0 +1,37 @@
+interface Reservation {
+  id: number;
+  hotelId: number;
+  userId: number;
+  roomId: number;
+  checkInDate: string;
+  checkOutDate: string;
+}
+
+const reservations: Reservation[] = [
+  {
+    id: 1,
+    hotelId: 1,
+    userId: 101,
+    roomId: 1,
+    checkInDate: "2024-01-01",
+    checkOutDate: "2024-01-05",
+  },
+  {
+    id: 2,
+    hotelId: 2,
+    userId: 102,
+    roomId: 2,
+    checkInDate: "2024-02-01",
+    checkOutDate: "2024-02-10",
+  },
+  {
+    id: 3,
+    hotelId: 1,
+    userId: 103,
+    roomId: 3,
+    checkInDate: "2024-03-01",
+    checkOutDate: "2024-03-05",
+  },
+];
+
+export default reservations;
